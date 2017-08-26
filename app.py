f1e043809e1608f584391da7cd6cf43ea2718699
Federico Ulfo <federicoulfo@gmail.com>
2017-08-26 00:54:45 -0400

    Add GET /chats/<user_id>

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 from flask_restful import Api
 
 from conf.config import HTTP_HOST, HTTP_PORT
-from service.chat import CreateChat
+from service.chat import CreateChat, GetChatList
 from service.healthz import Healthz
 
 app = Flask(__name__)
@@ -11,6 +11,7 @@ api = Api(app)
 
 api.add_resource(Healthz, '/healthz')
 api.add_resource(CreateChat, '/chat')
+api.add_resource(GetChatList, '/chats/<user_id>')
 
 if __name__ == '__main__':
     app.run(host=HTTP_HOST, port=HTTP_PORT)
--- service/chat.py
+++ service/chat.py
@@ -22,18 +22,24 @@ class CreateChat(Resource):
         if not user_ids or len(user_ids) == 0:
             return make_response("You must send the user ids", 500)
 
+        user_names = ', '.join(list(set(user_ids)))
+        chat_name = (user_names[:30] + '...') if len(user_names) > 30 else user_names
         chat_id = str(abs(hash(''.join(list(set(user_ids))))))
 
         connection.setup(hosts=CASSANDRA_HOSTS, default_keyspace=MESSAGE_KEYSPACE)
 
+        # todo: check if the chat_id for certain user already exists, if it does don't create it again
+
         for user_id in user_ids:
             ChatByChatId.create(
                 chat_id=chat_id,
-                user_id=user_id
+                user_id=user_id,
+                chat_name=chat_name,
             )
             ChatByUserId.create(
                 chat_id=chat_id,
-                user_id=user_id
+                user_id=user_id,
+                chat_name=chat_name,
             )
             chat_object = {
                 "chat_id": chat_id,
@@ -57,3 +63,21 @@ class CreateChat(Resource):
         }
         topic.publish(json.dumps(message_grammar))
         return True
+
+
+class GetChatList(Resource):
+    def get(self, user_id):
+        connection.setup(hosts=CASSANDRA_HOSTS, default_keyspace=MESSAGE_KEYSPACE)
+
+        chat_rows = ChatByUserId.filter(user_id=user_id)
+
+        chat_list = []
+        for chat in chat_rows:
+            chat_list.append(chat.to_object())
+
+        return {
+            "tot": len(chat_list),
+            "requests": chat_list
+        }
+
+
